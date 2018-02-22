9b181684764643cb48e0316665faf00f17b42454
ShittulPatel <shpatel@utilitywarehouse.co.uk>
2018-02-22 12:00:52 +0000

    Adding numbers lesson

---
+++ 02_numbers_sytem/binary/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Printf("%d - %b \n", 52, 52)
+
+  //%d - Decimal
+  //%b - Binary
+}
---
+++ 02_numbers_sytem/hexa/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  fmt.Printf("%d \t %b \t %#X \n", 45, 45, 45)
+  // 	fmt.Printf("%d - %b - %#x \n", 45, 45, 45)
+}
