9e8ba3f30e9460da91047d0cbdeeea17c3b4d55f
ShittulPatel <shpatel@utilitywarehouse.co.uk>
2018-03-29 12:13:31 +0100

    update

--- Exercise/randomrecordsgen.go
+++ Exercise/randomrecordsgen.go
@@ -4,22 +4,34 @@ import (
 	"fmt"
 	"github.com/icrowley/fake"
 	"strings"
+	"os"
 )
 
 const (
-	text1 = "****Persona Generator****"
-	text2 = "-*-*-*-*-*-*-*-*-*-*-*-*-"
-	text3 = "Please enter a number:"
+	text1   = "****Persona Generator****"
+	text2   = "-*-*-*-*-*-*-*-*-*-*-*-*-"
+	text3   = "Please enter a number:"
+	text4   = "Total Number of Records:"
+
 )
+//var (
+//	minNum     = 1
+//	maxNum     = 10
+//)
+
 func main() {
 
     r:= returnnum()
 	    fmt.Println(text1)
+
     for i := 0; i < r; i++ {
-		fmt.Println(persona(),"\n")
+    	fmt.Println(persona(), "\n")
 		fmt.Println("EOF Record Number:", i + 1)
 		fmt.Println(text2)
 	}
+	//fmt.Printf("Contains United States of America %d ", strings.Count(til, "United States of America"))
+	//fmt.Printf("Contains United Kingdom %d ", strings.Count(persona(), "United Kingdom"))
+	fmt.Println(text4,r)
 }
 
 func persona() string {
@@ -33,15 +45,20 @@ func persona() string {
 	honum := fake.Phone()
 	monum := fake.DigitsN(11)
 
-	return "\n" + "NAME: " + fullname + "\n" + "Email Address: " + email + "\n" + "Street Address: " + sadd + "\n" + "Zip: " + padd + "\n" + "State: "+
+	return "\n" + "NAME: " + fullname + "\n" + "Email Address: " +
+		email + "\n" + "Street Address: " + sadd + "\n" + "Zip: " + padd + "\n" + "State: "+
 		cadd + "\n" + "Country: " + coadd + "\n" + "H Phone: " + honum + "\n" + "Mobile: " + monum
 }
 
 func returnnum() int {
-	fmt.Print(text3)
 	var num int
-	fmt.Scan(&num)
- return num
+	fmt.Print(text3)
+	n, err := fmt.Scan(&num)
+	if err != nil || n != 1 {
+		fmt.Println(n, err)
+		os.Exit(1)
+	}
+    return num
 }
 
 
